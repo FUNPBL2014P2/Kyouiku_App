183d931ec5bcdeaa1cdcabb09f7d0e980342da49
b1012011 <b1012011@fun.ac.jp>
2014-12-08 16:03:39 +0900

    morohara order resolved

--- EV3Controller/src/com/example/ev3controller/EV3Materials.java
+++ EV3Controller/src/com/example/ev3controller/EV3Materials.java
@@ -6,21 +6,21 @@ import android.widget.ImageView;
 public class EV3Materials extends Application {
 	public EV3 ev3;
 	public ImageView[] blocks = new ImageView[8];
-	private int Threadstatus = 0; //0がoff,1がon
-	private int Functionstatus = 0; //初期値0(top),ラジコン1,プログラミング2
+	public static int Threadstatus = 0; //0がoff,1がon
+	public static int Functionstatus = 0; //初期値0(top),ラジコン1,プログラミング2
 	
 	
-	int getThreadstatus(){
+	public static int getThreadstatus(){
 		return Threadstatus;
 	}
-	public void setThreadstatus(int n){
+	public static void setThreadstatus(int n){
 		Threadstatus = n;
 	}
 	
-	int getFunctionstatus(){
+	public static int getFunctionstatus(){
 		return Functionstatus;
 	}
-	public void setFunctionstatus(int n){
+	public static void setFunctionstatus(int n){
 		Functionstatus = n;
 	}
 	
